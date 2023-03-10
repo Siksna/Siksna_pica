7bbc66a251a38b914b67a32968d79598a78adfee
Siksna <ulfsssiksna@gmail.com>
2023-03-10 09:17:13 +0200

    Izveidota switch izvele un PicasPasutijums metode kura tika iesākta

--- src/DominoPicerija.java
+++ src/DominoPicerija.java
@@ -1,9 +1,39 @@
+import javax.swing.JOptionPane;
+
 
 public class DominoPicerija {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	static void PicasPasutijums(){
+String[] PicasVeids = {"Pica ar sieru","Pica ar annanasiem","Pepperoni","Margarita","Vezuva","Veìitârâ","Pica ar ðíiòíi","Pârlocîtâ"};
+      int IzveletaisPicasVeids = (int) JOptionPane.showOptionDialog(null, "Izvçlies picas veidu","Izvçle",0,3,null,PicasVeids,PicasVeids[0]);
 	}
 
+	public static void main(String[] args) {
+		int metodesIndekss;
+	
+		
+		String[] switchMetodes = {"Pasûtît picu","Apskatît picas sastâvdaïas","Apskatît èeku","Apskatît citus pircçjus rindâ","Beigt darbu."};
+		
+		do{
+       metodesIndekss = (int) JOptionPane.showOptionDialog(null, "Izvçlies darbîbu","Izvçle",0,3,null,switchMetodes,switchMetodes[0]);
+				
+				switch(metodesIndekss){
+				case 0:
+					PicasPasutijums();
+					break;
+				case 1:
+					
+					break;
+				case 2:
+					
+					break;
+				case 3:
+					
+					break;
+				case 4:
+					
+					break;
+	}
+		}while(metodesIndekss!=4);
+}
 }
