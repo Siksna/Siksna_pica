751adf064f6cec37eee2766856b359cbe486f648
Siksna <ulfsssiksna@gmail.com>
2023-03-10 09:21:03 +0200

    Ideja par vairaku ievades iespeju 1 lodziņā

--- src/DominoPicerija.java
+++ src/DominoPicerija.java
@@ -1,9 +1,33 @@
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
-
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class DominoPicerija {
 	
 	static void PicasPasutijums(){
+		/*
+		JPanel myPanel = new JPanel(new GridBagLayout());
+		JTextField locationField = new JTextField(10);
+	    JTextField usedByField = new JTextField(5);
+	    JTextField commentField = new JTextField(50);
+
+	   
+
+	    myPanel.add(new JLabel("Location: ");
+	    myPanel.add(locationField);
+
+	    myPanel.add(new JLabel("Used By: ");
+	    myPanel.add(usedByField);
+
+	    myPanel.add(new JLabel("Comments: ");
+	    myPanel.add(commentField);
+
+	   
+
+	    JOptionPane.showInputDialog(myPanel);
+		*/
+		
 String[] PicasVeids = {"Pica ar sieru","Pica ar annanasiem","Pepperoni","Margarita","Vezuva","Veìitârâ","Pica ar ðíiòíi","Pârlocîtâ"};
       int IzveletaisPicasVeids = (int) JOptionPane.showOptionDialog(null, "Izvçlies picas veidu","Izvçle",0,3,null,PicasVeids,PicasVeids[0]);
 	}
